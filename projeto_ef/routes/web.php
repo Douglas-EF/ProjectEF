cd9207234fc8c9550f050a853743ad2d61780ce3
Douglas-EF <douglas@eficienciafiscal.com.br>
2021-05-18 19:50:33 -0400

    Atualização 1.0.0

--- projeto_ef/routes/web.php
+++ projeto_ef/routes/web.php
@@ -2,17 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('menu');
 });
+
+Route::get('/superior_direto', function () {
+    return view('superior_direto_prod');
+});
+
+Route::get('/usuarios', [Controllers\UsuariosController::class, 'index']);
+
+Route::get('/estoque', [Controllers\EstoqueController::class, 'index']);
+
+Route::get('/compra', [Controllers\ListaComprasController::class, 'index']);
+
+Route::get('/controles', [Controllers\ListaControlesController::class, 'index']);
+
+Route::get('/cotacoes', [Controllers\CotacoesController::class, 'index']);
+
+Route::get('/produtos', [Controllers\ProdutosController::class, 'index']);
+
+// ROUTS FOR ADD
+Route::get('/produtos/create', [Controllers\ProdutosController::class, 'create'])->name('produtos.create');
+Route::post('/produtos/', [Controllers\ProdutosController::class, 'store'])->name('produtos.store');
